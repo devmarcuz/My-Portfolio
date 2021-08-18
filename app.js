0e9efae84364f5843955b482258cb9dae1a7de75
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-08-18 02:58:57 +0100

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const path = requrie("path")
+const path = require("path")
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.static("/client/build"));
+app.use(express.static("client/build"));
 app.get("*", (req, res) =>
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 );
